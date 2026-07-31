1465561770802233a0fcd131aa58bf628616f33a
Rohit <rkp7133@gmail.com>
2026-07-31 15:47:12 +0530

    added server

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,57 @@
-//
+import { useState } from "react";
 
-// added commment
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
-    <div className="bg-blue-700 w-full h-17.5">
+      <div className="bg-blue-700 w-full h-17.5 flex">
+        {/*LOGO | Search Bar | Menu | User Statu (login , user , sgin up)*/}
+        {/**/}
+        {/*First div for logo and search bar */}
+        <div className="flex">
+          <div>
+            <img src="" alt="TBV" />
+          </div>
+          <div>
+            <input type="search" name="" id="" placeholder="Search" />
+          </div>
+        </div>
 
-    </div>
+        {/**/}
+        {/**/}
+        {/**/}
+
+        {/*Second div for menu and login status */}
+        <div className="flex">
+          <div>
+            <button aria-expanded={isOpen} onClick={() => setIsOpen(!isOpen)}>
+              Menu
+            </button>
+            {isOpen && (
+              <ul aria-label="Menu">
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="#">DashBoard</a>
+                </li>
+                <li>
+                  <a href="#">About</a>
+                </li>
+                <li>
+                  <a href="#">Contact</a>
+                </li>
+              </ul>
+            )}
+          </div>
+          <div>
+            <button>Login</button>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
